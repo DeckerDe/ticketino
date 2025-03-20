8038a612618d466c6157a53ebcfe088e6b40796a
DeckerDe <Deiwidecker@gmail.com>
2025-03-19 22:20:36 -0300

    initial

---
+++ src/app/data.ts
@@ -0,0 +1,26 @@
+export const initialTickets = [
+    {
+        id: "1",
+        title: 'Create a new project',
+        content: 'Create a new project using Angular CLI',
+        status: 'Open',
+    },
+    {
+        id: "2",
+        title: 'Setup Angular Material',
+        content: 'Setup Angular Material in the project',
+        status: 'Open',
+    },
+    {
+        id: "3",
+        title: 'Create components',
+        content: 'Create components for the project',
+        status: 'Open',
+    },
+    {
+        id: "4",
+        title: 'Create services',
+        content: 'Create services for the project',
+        status: 'Open',
+    },
+]
